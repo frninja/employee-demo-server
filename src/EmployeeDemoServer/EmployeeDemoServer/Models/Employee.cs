d7210398fd161a45d33071e6317359a35c9310b0
Oleg Batashov <krash.se.us@gmail.com>
2017-12-09 18:34:23 +0300

    Модель сотрудника - добавить валидацию

---
+++ src/EmployeeDemoServer/EmployeeDemoServer/Models/Employee.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace SimpleCode.EmployeeDemoServer.Models
+{
+    public class Employee
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        public string Name { get; set; }
+
+        // TODO: Validate email.
+        [Required]
+        public string Email { get; set; }
+
+        // TODO: Validate date.
+        [Required]
+        public DateTime BirthDay { get; set; }
+
+        // TODO: Validate > 0.
+        [Required]
+        public int Salary { get; set; }
+
+        public Employee() {}
+    }
+}
