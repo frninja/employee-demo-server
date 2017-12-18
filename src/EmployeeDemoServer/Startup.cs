ce5984c62052495fe396adcea5002286af7d8def
Oleg Batashov <krash.se.us@gmail.com>
2017-12-18 10:46:13 +0300

    OWIN

---
+++ src/EmployeeDemoServer/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Owin;
+using Microsoft.Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(SimpleCode.EmployeeDemoServer.Startup))]
+namespace SimpleCode.EmployeeDemoServer
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+
+            app.UseWebApi(config);
+        }
+    }
+}
